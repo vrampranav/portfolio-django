9518d20447fb808eb9a9c3a62dff18e3727e869e
Rampranav <vrampranav9@gmail.com>
2020-12-18 21:35:10 +0530

    Added Html file
    Remove Key

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello! Welcome!")
+    response = render(request,'main_app/index.html')
+    return response
     
 
